d815c1b6661f20ac6abaebebc869ec49a1d0824a
mxflrs <max@maxflores.dev>
2025-08-26 14:30:12 -0600

    chore: desktop layout

--- src/core/utils/insert-break.ts
+++ src/core/utils/insert-break.ts
@@ -1,6 +1,9 @@
-export function insertBreakAfter(text: string, word: string) {
+export function insertBreakAfter(text: string, word: string, className?: string) {
+  const spacer = className
+    ? `<span class="${className}" style="display:block"></span>`
+    : '<br />';
   return text.replace(
     new RegExp(`(${word})`),
-    '$1<br />'
+    `$1${spacer}`
   );
 }
